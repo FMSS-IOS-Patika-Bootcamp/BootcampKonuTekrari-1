819d79093283d2366052dded31f3ba130a984f7e
mertdemirtas <m.demirtas99@icloud.com>
2022-09-18 16:10:49 +0300

    Initial Commit

---
+++ BootcampKonuTekrari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BootcampKonuTekrari
+//
+//  Created by Mert Demirtaş on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
